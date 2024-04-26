c428a71c62ff4440f6c688c092861d9965cfbec4
System Administrator <root@Yonathans-MacBook-Pro.local>
2024-04-26 00:47:06 -0500

    Angular weather app

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -5,13 +5,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class WeatherService {
+  API_KEY = '482944e26d320a80bd5e4f23b3de7d1f';
   constructor(private http: HttpClient) {}
 
   getWeather(city: string) {
     return this.http.get(
       'https://api.openweathermap.org/data/2.5/forecast/?q=' +
         city +
-        '&appid=482944e26d320a80bd5e4f23b3de7d1f' +
+        '&appid=' +
+        this.API_KEY +
         '&units=metric'
     );
   }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-weather',
@@ -9,6 +10,8 @@ import { WeatherService } from '../weather.service';
 export class WeatherComponent implements OnInit {
   cities: string[] = ['Rio de Janeiro', 'Beijing', 'Los Angeles']; // List of cities
   forecasts: any[] = [];
+  maxTemps = new Array();
+  minTemps = new Array();
 
   constructor(private weatherService: WeatherService) {}
 
@@ -21,12 +24,42 @@ export class WeatherComponent implements OnInit {
       this.weatherService.getWeather(city).subscribe({
         next: (res: any) => {
           this.forecasts.push(res);
+          this.findMaxTemp();
+          this.findMinTemp();
         },
 
         error: (error: { message: any }) => console.log(error.message),
 
         complete: () => console.info('API call completed'),
       });
+    }
+  }
+  findMaxTemp() {
+    for (let i = 0; i < this.forecasts.length; i++) {
+      this.maxTemps[i] = new Array();
+      for (var j = 0; j < 5; j++) {
+        this.maxTemps[i].push(
+          Math.max(
+            ...this.forecasts[i].list
+              .slice(8 * j, 8 * (j + 1))
+              .map((o: { main: { temp_max: any } }) => o.main.temp_max)
+          )
+        );
+      }
+    }
+  }
+  findMinTemp() {
+    for (let i = 0; i < this.forecasts.length; i++) {
+      this.minTemps[i] = new Array();
+      for (var j = 0; j < 5; j++) {
+        this.minTemps[i].push(
+          Math.min(
+            ...this.forecasts[i].list
+              .slice(8 * j, 8 * (j + 1))
+              .map((o: { main: { temp_min: any } }) => o.main.temp_min)
+          )
+        );
+      }
     }
   }
 }
